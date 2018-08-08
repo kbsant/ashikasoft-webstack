38dac4bd60c903705baea6209d7554722ae88966
kbsant <keansant@gmail.com>
2018-08-08 12:17:42 +0900

    cli tools

---
+++ src/clj/ashikasoft/webstack/cli.clj
@@ -0,0 +1,16 @@
+(ns ashikasoft.webstack.cli
+  (:require [clojure.tools.cli :as cli]
+            [clojure.tools.logging :as log]
+            [integrant.core :as ig]))
+
+(def cli-options-config
+  "The command line option config, used by parse-opts."
+  [["-p" "--port PORT" "Port number"
+    :parse-fn #(Integer/parseInt %)]])
+
+(defn parse-options [args]
+  (or (cli/parse-opts args cli-options-config) {}))
+
+(defn add-shutdown-hook [stop-fn]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. #(stop-app system))))
+

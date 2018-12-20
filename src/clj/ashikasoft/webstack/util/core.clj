e31b0b06a699c28a8dddb0ed948f0ce82f7b7eef
kbsant <keansant@gmail.com>
2018-12-20 14:07:46 +0900

    add interpolate util

--- src/clj/ashikasoft/webstack/util/core.clj
+++ src/clj/ashikasoft/webstack/util/core.clj
@@ -1,6 +1,19 @@
 (ns ashikasoft.webstack.util.core
   (:require
-    [cheshire.core :as json]))
+   [clojure.string :as string]
+   [clojure.pprint :as pprint]
+   [cheshire.core :as json]))
+
+(defn interpolate
+  "Given an optional transform function, a string and a map, interpolate the string
+  with values from the map. The pattern {:keyname} will be replaced with values
+  from the map."
+  ([s m]
+   (interpolate identity s m))
+  ([xf s m]
+   (reduce #(string/replace %1 (str "{" %2 "}") (xf (str (%2 m))))
+           s
+           (keys m))))
 
 (defmulti export-format (fn [format _] format))
 
@@ -8,7 +21,7 @@
   (json/generate-string data {:pretty true}))
 
 (defmethod export-format "EDN" [_ data]
-  (clojure.pprint/write data :stream nil))
+  (pprint/write data :stream nil))
 
 (defn resource-edn [path]
   (-> path clojure.java.io/resource slurp clojure.edn/read-string))
---
+++ test/ashikasoft/webstack/util/core_test.clj
@@ -0,0 +1,23 @@
+(ns ashikasoft.webstack.util.core-test
+  (:require
+   [ashikasoft.webstack.util.core :refer :all]
+   [clojure.string :as string]
+   [clojure.test :refer :all]))
+
+(deftest test-interpolate
+  (testing "Interpolate a single entry."
+    (is (= "Rubberduckyyou'resofine"
+           (interpolate "Rubber{:x}you'resofine" {:x "ducky"}))))
+  (testing "Interpolate multiple entries."
+    (is (= "Rubberducky you're so fine number 9"
+           (interpolate "Rubber{:x} you're {:y} number {:z}"
+                        {:x 'ducky :y "so fine" :z 9}))))
+  (testing "Interpolate without entries."
+    (is (= "Rubbergatoryou'resofine"
+           (interpolate "Rubbergatoryou'resofine" {:x "ducky"}))))
+  (testing "Skip interpolation if a key is missing."
+    (is (= "Rubber{:x}you'resofine"
+           (interpolate "Rubber{:x}you'resofine" {:z "ducky"}))))
+  (testing "Skip interpolation of the map is nil."
+    (is (= "Rubber{:x}you'resofine"
+           (interpolate "Rubber{:x}you'resofine" nil)))))

4ca1e4453bae0a40097c570234c0c6eee7d2391e
kbsant <keansant@gmail.com>
2021-02-14 08:00:19 +0900

    bump deps and release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ashikasoft/webstack "0.1.3-SNAPSHOT"
+(defproject ashikasoft/webstack "0.1.4"
 
   :description "Base web stack"
   :url "https://github.com/kbsant/ashikasoft-webstack"
@@ -12,9 +12,9 @@
                  [luminus-immutant "0.2.5"]
                  [luminus-nrepl "0.1.7"]
                  [luminus/ring-ttl-session "0.3.3"]
-                 [metosin/muuntaja "0.6.7"]
-                 [metosin/reitit "0.5.11"]
-                 [metosin/ring-http-response "0.9.1"]
+                 [metosin/muuntaja "0.6.8"]
+                 [metosin/reitit "0.5.12"]
+                 [metosin/ring-http-response "0.9.2"]
                  [com.fasterxml.jackson.core/jackson-core "2.12.1"]
                  [org.apache.commons/commons-text "1.9"]
                  [org.clojure/clojure "1.10.2"]
@@ -23,12 +23,12 @@
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.webjars.bower/tether "1.4.4"]
-                 [org.webjars/bootstrap "4.1.1"]
+                 [org.webjars/bootstrap "4.6.0"]
                  [org.webjars/font-awesome "5.15.2"]
                  [org.webjars/webjars-locator "0.40"]
                  [reagent "1.0.0"]
                  [ring-webjars "0.2.0"]
-                 [ring/ring-core "1.8.2"]
+                 [ring/ring-core "1.9.0"]
                  [ring/ring-defaults "0.3.2"]]
 
   :source-paths ["src/clj" "src/cljc"]
